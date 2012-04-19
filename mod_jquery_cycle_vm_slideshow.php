94339c09af44987cabb23146bd972c060121c7e9
Ronak Jain <ronakjain90@gmail.com>
2012-04-19 23:26:49 +0530

    XML corrected, PHP updated

--- mod_jquery_cycle_vm_slideshow.php
+++ mod_jquery_cycle_vm_slideshow.php
@@ -1,37 +1,25 @@
 <?php
-//don't allow other scripts to grab and execute our file
-defined('_JEXEC') or die('Direct Access to this location is not allowed.');
-
-if (!class_exists( 'VmConfig' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'config.php');
-VmConfig::loadConfig();
-// Load the language file of com_virtuemart.
-JFactory::getLanguage()->load('com_virtuemart');
-if (!class_exists( 'calculationHelper' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'calculationh.php');
-if (!class_exists( 'CurrencyDisplay' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'currencydisplay.php');
-if (!class_exists( 'VirtueMartModelVendor' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'models'.DS.'vendor.php');
-if (!class_exists( 'VmImage' )) require(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart'.DS.'helpers'.DS.'image.php');
-if (!class_exists( 'shopFunctionsF' )) require(JPATH_SITE.DS.'components'.DS.'com_virtuemart'.DS.'helpers'.DS.'shopfunctionsf.php');
-if (!class_exists( 'calculationHelper' )) require(JPATH_COMPONENT_SITE.DS.'helpers'.DS.'cart.php');
-if (!class_exists( 'VirtueMartModelProduct' )){
-   JLoader::import( 'product', JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_virtuemart' . DS . 'models' );
-}
-$mainframe = Jfactory::getApplication();
-$virtuemart_currency_id = $mainframe->getUserStateFromRequest( "virtuemart_currency_id", 'virtuemart_currency_id',JRequest::getInt('virtuemart_currency_id',0) );
+defined('_JEXEC') or die();
 if(!class_exists('VmModel'))require(JPATH_VM_ADMINISTRATOR.DS.'helpers'.DS.'vmmodel.php');
-echo JPATH_VM_ADMINISTRATOR.DS.'helpers'.DS.'vmmodel.php';
+if (!class_exists('VmMediaHandler')) require(JPATH_VM_ADMINISTRATOR.DS.'helpers'.DS.'mediahandler.php');
+
+//echo JPATH_VM_ADMINISTRATOR.DS.'helpers'.DS.'vmmodel.php';
+
 ?>
 <script type="text/javascript">
 jQuery(document).ready(function(){
-alert("hi");
+//alert("hi");
 })
 </script>
 <p>
-    Hello World
+    Hello orld
 </p>
 <?php 
-	$productModel = VmModel::getModel('Product');
-	$pid = new ArrayObject(1,2);
-	$product= $productModel->getProducts();
+	$model = VmModel::getModel('Media');
+	$list = array();
+	$pid = array('1','2');
+	$list['images'] = $model->createMediaByIds($pid,'');
+	echo $list->file_url ."Hello";
 	
 	
 ?>
