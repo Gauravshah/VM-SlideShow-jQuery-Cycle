9fbb3d94bd19e1ef65c47581b8145d711fd7e80b
Ronak Jain <ronakjain90@gmail.com>
2012-04-25 23:30:49 +0530

    Display Parameters added in the XML file, Parameters Successfully grouped, PHP file some error

--- mod_jquery_cycle_vm_slideshow.php
+++ mod_jquery_cycle_vm_slideshow.php
@@ -12,13 +12,15 @@ if(!class_exists('VmModel'))require(JPATH_VM_ADMINISTRATOR.DS.'helpers'.DS.'vmmo
 		require(JPATH_VM_ADMINISTRATOR . DS . 'models' . DS . 'media.php');
 		
 		$attributes_map = Array(
-		'productName'=>'product_name',
-		'productWeightType' => "product_weight_uom",
-		"product_weight" => "product_weight",
-		"product_length" => "product_length",
-		"product_in_stock" => "product_in_stock",
-		"product_sales" => "product_sales",
-		"category_name" => "category_name",
+		"productName"=>"product_name",
+		"productWeightType" => "product_weight_uom",
+		"productWeight" => "product_weight",
+		"productLength" => "product_length",
+		"productInStock" => "product_in_stock",
+		"productSales" => "product_sales",
+		"categoryName" => "category_name",
+		"productPrice" => "product_price",
+		
 		);
 ?>
 <script type="text/javascript">
@@ -34,8 +36,8 @@ jQuery(document).ready(function(){
 
 	$id = split(',',$params->get('productIds'));
 	$product= $productModel->getProducts($id);
-		$productModel->addImages($product);
-
+	
+	$productModel->addImages($product);
 	$mediaModel = VmModel::getModel('Media');
 
 ?>
@@ -44,22 +46,63 @@ jQuery(document).ready(function(){
 <?php
 foreach($product as $pro)
 {
-
+//print_r($pro);
 $images = $mediaModel->createMediaByIds($pro->virtuemart_media_id);
 
 	?>	
     <div id="slideshow" >  
-    
+			
     
 					<div class="cycle">
 						<img alt="" src="<?php echo $images[0]->file_url; ?>">
 						<div class="farme-slide-text">
 							<ul class="slide-text">
-                          <?php 
-						  foreach($attributes_map as $k=>$v)
-						  {?>  
-                           <li><span class="left"><?php echo $k?></span> <?php echo $pro->$v ?></li>
-                                <?php }?>
+							<?php 
+							foreach($attributes_map as $k=>$v)
+							{
+								$flag=0;
+								if  ($params->get('productName') == 'yes')
+								{
+									$flag=1;
+								}
+								else if($params->get('productWeightType') == 'yes')
+								{
+									$flag=1;
+								}
+								else if($params->get('productWeight') == 'yes')
+								{
+									$flag=1;
+								}
+								else if($params->get('productLength') == 'yes')
+								{
+									$flag=1;
+								}
+								else if($params->get('productInStock') == 'yes') 
+								{
+									$flag=1;
+								}
+								else if($params->get('productSales') == 'yes') 							
+								{
+									$flag=1;
+								}
+								else if($params->get('productPrice') == 'yes') 	
+								{
+									$flag=1;
+								}
+								else if($params->get('categoryName') == 'yes')
+								{
+									$flag=1;
+								}
+								else
+								{
+									$flag=0;
+								
+								}
+								if($flag=1)
+								{?>  
+										<li><span class="left"><?php echo $k?></span> <?php echo $pro->$v ?></li>
+						<?php	}
+							}?>
 							</ul>
 							<div class="frame-price">
 								<div class="slider-button"><a href="<?php echo $pro->link ?>">info</a></div>
